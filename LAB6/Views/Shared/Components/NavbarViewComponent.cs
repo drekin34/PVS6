57b8bdb74b94500160eadfd94d5628184286f658
drekin34 <dmitroslobodan@gmail.com>
2024-11-09 02:45:54 +0200

    first

---
+++ LAB6/Views/Shared/Components/NavbarViewComponent.cs
@@ -0,0 +1,13 @@
+﻿namespace LAB6.Views.Shared.Components
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class NavbarViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+
+}
